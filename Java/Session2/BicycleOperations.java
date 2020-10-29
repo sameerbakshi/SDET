46d8bbfc341acfa10f6fc866abb175425f2b1405
sameerbakshi <73516609+sameerbakshi@users.noreply.github.com>
2020-10-29 10:43:48 +0530

    Add files via upload

---
+++ Java/Session2/BicycleOperations.java
@@ -0,0 +1,6 @@
+package JavaActivity3;
+
+interface BicycleOperations {
+	public void applyBrake(int decrement);
+    public void speedUp(int increment);
+}
---
+++ Java/Session2/BicycleParts.java
@@ -0,0 +1,6 @@
+package JavaActivity3;
+
+interface BicycleParts {
+	public int gears=0;
+	public int currentSpeed=0;
+}
