bcc097c64e3db7d6955f4dc3be2d191bd0e24039
sameerbakshi <73516609+sameerbakshi@users.noreply.github.com>
2020-12-09 00:00:04 +0530

    Add files via upload

---
+++ Cucumber/src/test/java/cucumberTest/ActivitiesRunner2_6_1.java
@@ -0,0 +1,18 @@
+package cucumberTest;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"pretty"},
+    monochrome = true
+)
+public class ActivitiesRunner2_6_1 {
+
+}
